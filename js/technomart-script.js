24a7ec5d9ce7ced9c7737d14d309efb180030c01
KuryakovDen <fifer2@yandex.ru>
2019-06-21 22:09:41 +0700

    Revert "подключение JS"
    This reverts commit 32e8d16a70653b019eca3f594d0069da5f0a32c9.

--- js/technomart-script.js
+++ js/technomart-script.js
@@ -1,14 +1,1 @@
-var mapLink = document.querySelector(".go-to-map");
-var modalMapWindow = document.querySelector(".modal-map-window");
-var closeMapWindow = document.querySelector(".modal-map-window::after");
 
-
-mapLink.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalMapWindow.classList.add("modal-show");
-});
-
-closeMapWindow.addEventListener("", function () {
-    evt.preventDefault();
-    modalMapWindow.classList.remove("modal-show");
-});
